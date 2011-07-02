4105606278d733ad1f04160dfe8e8cf42175b03b
derariii@gmail.com <derariii@gmail.com@7f30eca5-7aae-187b-1e34-929928709822>
2011-07-02 20:37:42 +0000

    towards .rmskin file

--- trunk/RainmeterOutlook/deploy.example.bat
+++ trunk/RainmeterOutlook/deploy.example.bat
@@ -9,13 +9,18 @@ SET RAINMETER-PLUGINS=%RAINMETER-ROOT%Plugins\
 
 ECHO Stopping Rainmeter
 START "" /WAIT "%RAINMETER%" !RainmeterQuit
-: wait 2 seconds for rainmeter to close
-@ping -n 2 localhost> nul
+: wait 3 seconds for rainmeter to close
+@ping -n 3 localhost> nul
 
+: copy files to rmskin template
+COPY _compiled\x64\OutlookPlugin.dll Template\Plugins\64bit> nul
+COPY _compiled\x32\OutlookPlugin.dll Template\Plugins\32bit> nul
+DEL Template\Skins /Q
+XCOPY Skins\* Template\Skins /S /V /Q /Y> nul
+
+: copy files to rainmeter
 ECHO Copying files
-: copy plugin file
 COPY _compiled\x%BIT%\OutlookPlugin.dll "%RAINMETER-PLUGINS%"> nul
-: copy skin files
 XCOPY Skins\* "%RAINMETER-SKINS%" /S /V /Q /Y> nul
 
 ECHO Restarting Rainmeter
